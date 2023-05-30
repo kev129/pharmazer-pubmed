dbaf9f69d131920c5f4d2bd237969559b3c8bf87
Kevin <kevinhnguyen9@gmail.com>
2023-05-30 11:17:49 +0100

    Added Function to get element text

--- data_exploration.py
+++ data_exploration.py
@@ -1,4 +1,6 @@
 import xml.etree.ElementTree as et
+from typing import Optional
+from xml.etree.ElementTree import Element
 
 xml_data_path = "./data/pubmed_result_sjogren.xml"
 
@@ -12,3 +14,15 @@ articles = pubmed_xml_root.findall("./PubmedArticle")
 
 articles_count = len(articles)
 print(articles_count)
+
+def get_element_text(element: Element) ->Optional[str]:
+    """Gets the text content of the given XML element
+
+    Args:
+        element (Element): XML Element that contains text to be retrieved
+
+    Returns:
+        Optional[Element]: Text content, or none if not available
+    """
+    return element.text or 'None'
+
