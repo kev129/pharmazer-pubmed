cede9c118d75150fb452f39321ca7989476553d3
Kevin <kevinhnguyen9@gmail.com>
2023-05-30 11:24:52 +0100

    Added Function to extract article data

--- data_exploration.py
+++ data_exploration.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as et
 from typing import Optional
-from xml.etree.ElementTree import Element
+
 
 xml_data_path = "./data/pubmed_result_sjogren.xml"
 
@@ -15,7 +15,7 @@ articles = pubmed_xml_root.findall("./PubmedArticle")
 articles_count = len(articles)
 print(articles_count)
 
-def get_element_text(element: Element) ->Optional[str]:
+def get_element_text(element: et.Element) ->Optional[str]:
     """Gets the text content of the given XML element
 
     Args:
@@ -26,3 +26,32 @@ def get_element_text(element: Element) ->Optional[str]:
     """
     return element.text or 'None'
 
+def extract_article_data(article_no: int, root: et.Element) -> dict[str]:
+    """Extracts article data, from xml file given an article number
+
+    Args:
+        article_no (int): Index of article in XML File
+        root (et.Element): Root element of XML Tree
+
+    Returns:
+        dict[str]: A dictionary containing the extracted data:
+            - 'title' (str): The title of the article
+            - 'pmid' (str): The PMID (PubMed ID) of the article
+            - 'year' (str): The publication year of the article
+            - 'keyword_list' (List[str]): List of keywords associated with the article
+            - 'mesh_list' (List[str]): List of Mesh Headings associated with the article
+    """
+    article = root[article_no]
+    title = article.find('.//ArticleTitle').text
+    pmid = article.find('.//PMID').text
+    year = article.find(".//PubDate/Year").text
+    keyword_list = [keyword.text for keyword in article.findall('.//Keyword')]
+    mesh_list = [descriptor.get('UI') for descriptor in article.findall(".//MeshHeading/DescriptorName")]
+    
+    return {
+        "title": title,
+        "pmid": pmid,
+        "year": year,
+        "keyword_list": keyword_list,
+        "mesh_list": mesh_list
+    }
